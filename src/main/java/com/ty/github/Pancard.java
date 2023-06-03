4803c98f3ecb683d686778c7c9601a386a2a78f5
Akshay8088 <akshaychavadi@gmail.com>
2023-06-03 12:00:03 +0530

    Person class

---
+++ src/main/java/com/ty/github/Pancard.java
@@ -0,0 +1,33 @@
+package com.ty.github;
+
+public class Pancard {
+private int id;
+private String name;
+private String address;
+private String panno;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public String getPanno() {
+	return panno;
+}
+public void setPanno(String panno) {
+	this.panno = panno;
+}
+
+}
---
+++ src/main/java/com/ty/github/Person.java
@@ -0,0 +1,58 @@
+package com.ty.github;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Person {
+	
+	private int id;
+	private String name;
+	private String address;
+	private long phone;
+	
+	@OneToOne
+	private Pancard pan;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+
+	public Pancard getPan() {
+		return pan;
+	}
+
+	public void setPan(Pancard pan) {
+		this.pan = pan;
+	}
+	
+
+}
